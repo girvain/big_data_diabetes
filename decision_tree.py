bd5e922cae0a6a71107e109b7ce1197ed8912806
girvain <gavinross88@hotmail.co.uk>
2019-12-10 15:28:44 +0000

    final iteration of code before checking in class

--- decision_tree.py
+++ decision_tree.py
@@ -46,7 +46,7 @@ print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
 # Create Decision tree classifier object
-clf = DecisionTreeClassifier(criterion="entropy", max_depth=8)
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=6)
 # train decision tree classifier
 clf = clf.fit(X_train, y_train)
 
@@ -55,23 +55,4 @@ y_pred = clf.predict(X_test)
 
 # model accuracy, how often is the classifier correct?
 print("accuracy post tuning:", metrics.accuracy_score(y_test, y_pred))
-
-# ------ Visualising the tree --------
-# from sklearn.tree import export_graphviz
-# from sklearn.externals.six import StringIO
-# from IPython.display import Image
-# import pydotplus
-
-# feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
-# dot_data = StringIO()
-# export_graphviz(clf, out_file=dot_data,
-#                 filled=True, rounded=True,
-#                 special_characters=True,
-#                 feature_names = feature_cols,
-#                 class_names=['0', '1'])
-
-# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-# graph.write_png('diabetes.png')
-# Image(graph.create_png())
-
 
